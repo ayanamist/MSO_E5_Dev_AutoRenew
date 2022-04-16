35367c867b8a13d4ef632e6a89066cec013cacca
klrst <kylejohnmarky075@gmail.com>
2022-04-16 21:01:52 +0800

    Updated main.py for english translation
    Translated from Traditional Chinese to English

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # -*- coding: UTF-8 -*-
 import requests as req
 import json,sys,time
-#先注册azure应用,确保应用有以下权限:
+#Register the azure app first and make sure the app has the following permissions:
 #files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
 #user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
 #mail:  Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
-#注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
+#After registration, you must click on behalf of xxx to grant administrator consent, otherwise outlook api cannot be called
 
 
 
@@ -45,38 +45,38 @@ def main():
     try:
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
             num1+=1
-            print("1调用成功"+str(num1)+'次')
+            print("1st Call successful"+str(num1)+'times')
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code == 200:
             num1+=1
-            print("2调用成功"+str(num1)+'次')
+            print("2nd Call successful"+str(num1)+'times')
         if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
             num1+=1
-            print('3调用成功'+str(num1)+'次')
+            print('3rd Call successful'+str(num1)+'times')
         if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
             num1+=1
-            print('4调用成功'+str(num1)+'次')
+            print('4th Call successful'+str(num1)+'times')
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
             num1+=1
-            print('5调用成功'+str(num1)+'次')    
+            print('5th Call successful'+str(num1)+'times')    
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
-            print('6调用成功'+str(num1)+'次')    
+            print('6th Call successful'+str(num1)+'times')    
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
-            print('7调用成功'+str(num1)+'次')
+            print('7th Call successful'+str(num1)+'times')
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次')
+            print('8th Call successful'+str(num1)+'times')
         if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次') 
+            print('8th Call successful'+str(num1)+'times') 
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
             num1+=1
-            print('9调用成功'+str(num1)+'次')
+            print('9th Call successful'+str(num1)+'times')
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
-            print('10调用成功'+str(num1)+'次')
-            print('此次运行结束时间为 :', localtime)
+            print('10th Call successful'+str(num1)+'times')
+            print('The end of this run is :', localtime)
     except:
         print("pass")
         pass
