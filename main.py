ee6288b3420d24a3b2983fc61ffdbbed36eaf0cc
Kyle Cabangon <kjmcabangon@slsu.edu.ph>
2023-03-11 21:23:28 +0800

    Update main.py
    code cleaning

--- main.py
+++ main.py
@@ -1,86 +1,67 @@
-# -*- coding: UTF-8 -*-
-import requests as req
-import json,sys,time
-#Register the azure app first and make sure the app has the following permissions:
-#files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
-#user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
-#mail:  Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
-#After registration, you must click on behalf of xxx to grant administrator consent, otherwise outlook api cannot be called
+import requests
+import json
+import time
+
+# Register the azure app first and make sure the app has the following permissions:
+# files: Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
+# user: User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
+# mail: Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
+# After registration, you must click on behalf of xxx to grant administrator consent, otherwise outlook api cannot be called
 
 
 
 
 
 
+endpoints = [
+    'https://graph.microsoft.com/v1.0/me/drive/root',
+    'https://graph.microsoft.com/v1.0/me/drive',
+    'https://graph.microsoft.com/v1.0/drive/root',
+    'https://graph.microsoft.com/v1.0/users',
+    'https://graph.microsoft.com/v1.0/me/messages',
+    'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+    'https://graph.microsoft.com/v1.0/me/drive/root/children',
+    'https://api.powerbi.com/v1.0/myorg/apps',
+    'https://graph.microsoft.com/v1.0/me/mailFolders',
+    'https://graph.microsoft.com/v1.0/me/outlook/masterCategories'
+]
 
-#path=sys.path[0]+r'/Secret.txt'
-num1 = 0
-
-def gettoken(refresh_token):
-    headers={'Content-Type':'application/x-www-form-urlencoded'
-            }
-    data={'grant_type': 'refresh_token',
-          'refresh_token': refresh_token,
-          'client_id':id,
-          'client_secret':secret,
-          'redirect_uri':'http://localhost:53682/'
-         }
-    html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
+def get_access_token(refresh_token, client_id, client_secret):
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token,
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'redirect_uri': 'http://localhost:53682/'
+    }
+    html = requests.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
     jsontxt = json.loads(html.text)
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
-    #with open(path, 'w+') as f:
-        #f.write(refresh_token)
     return access_token
+
 def main():
-    #fo = open(path, "r+")
-    #refresh_token = fo.read()
-    #fo.close()
-    print(refresh_token)
-    global num1
-    localtime = time.asctime( time.localtime(time.time()) )
-    access_token=gettoken(refresh_token)
-    headers={
-    'Authorization':access_token,
-    'Content-Type':'application/json'
-    }
-    try:
-        if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
-            num1+=1
-            print("1st Call successful"+str(num1)+'times')
-        if req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code == 200:
-            num1+=1
-            print("2nd Call successful"+str(num1)+'times')
-        if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
-            num1+=1
-            print('3rd Call successful'+str(num1)+'times')
-        if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
-            num1+=1
-            print('4th Call successful'+str(num1)+'times')
-        if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
-            num1+=1
-            print('5th Call successful'+str(num1)+'times')    
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
-            num1+=1
-            print('6th Call successful'+str(num1)+'times')    
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
-            num1+=1
-            print('7th Call successful'+str(num1)+'times')
-        if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
-            num1+=1
-            print('8th Call successful'+str(num1)+'times')
-        if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
-            num1+=1
-            print('8th Call successful'+str(num1)+'times') 
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
-            num1+=1
-            print('9th Call successful'+str(num1)+'times')
-        if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
-            num1+=1
-            print('10th Call successful'+str(num1)+'times')
-            print('The end of this run is :', localtime)
-    except:
-        print("pass")
-        pass
+    access_token = get_access_token(refresh_token, client_id, client_secret)
+    session = requests.Session()
+    session.headers.update({
+        'Authorization': access_token,
+        'Content-Type': 'application/json'
+    })
+    num = 0
+    for endpoint in endpoints:
+        try:
+            response = session.get(endpoint)
+            if response.status_code == 200:
+                num += 1
+                print(f'{num}th Call successful')
+        except requests.exceptions.RequestException as e:
+            print(e)
+            pass
+    localtime = time.asctime(time.localtime(time.time()))
+    print('The end of this run is :', localtime)
+
 for _ in range(3):
     main()
